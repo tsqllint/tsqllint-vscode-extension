8be07511789c51e278270f03a83fddb4d4984c6d
Otto Streifel <ottost@microsoft.com>
2018-01-22 10:20:00 -0800

    simplify build (#14)

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -7,51 +7,51 @@ import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind } f
 
 export function activate(context: ExtensionContext) {
 
-  const debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
+	let debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] }; 
 
-  const serverModule = context.asAbsolutePath(path.join("out/server/src", "server.js"));
+	let serverModule = context.asAbsolutePath(path.join('./server/out/src', 'server.js'));
 
-  const serverOptions: ServerOptions = {
-      run : { module: serverModule, transport: TransportKind.ipc },
-    debug: { module: serverModule, transport: TransportKind.ipc, options: debugOptions },
-  };
+	let serverOptions: ServerOptions = {
+		run : { module: serverModule, transport: TransportKind.ipc },
+		debug: { module: serverModule, transport: TransportKind.ipc, options: debugOptions }
+	}
 
-  const clientOptions: LanguageClientOptions = {
-    documentSelector: [{scheme: "file", language: "sql"}],
-    synchronize: {
-      configurationSection: "tsqllint",
-      fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
-    },
-  };
+	let clientOptions: LanguageClientOptions = {
+		documentSelector: [{scheme: 'file', language: 'sql'}],
+		synchronize: {
+			configurationSection: 'tsqllint',
+			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
+		}
+	}
+	
+	let client = new LanguageClient('tsqllint', 'TSQLLint', serverOptions, clientOptions);
+	client.registerProposedFeatures();
 
-  const client = new LanguageClient("tsqllint", "TSQLLint", serverOptions, clientOptions);
-  client.registerProposedFeatures();
-
-  function applyTextEdits(uri: string, documentVersion: number, edits: vscode.TextEdit[]) {
-    const textEditor = vscode.window.activeTextEditor;
-    if (textEditor && textEditor.document.uri.toString() === uri) {
-      if (textEditor.document.version !== documentVersion) {
-        vscode.window.showInformationMessage(
-          `SqlLint fixes are outdated and can't be applied to the document.`,
-        );
-      }
-      textEditor.edit((mutator) => {
-        for (const edit of edits) {
-          mutator.replace(client.protocol2CodeConverter.asRange(edit.range), edit.newText);
-        }
-      }).then((success) => {
-        if (!success) {
-          vscode.window.showErrorMessage(
-            "Failed to apply SqlLint fixes to the document. " +
-            "Please consider opening an issue with steps to reproduce.",
-          );
-        }
-      });
-    }
-  }
-
-  context.subscriptions.push(
-    client.start(),
-    vscode.commands.registerCommand("_tsql-lint.change", applyTextEdits),
-  );
+	
+function applyTextEdits(uri: string, documentVersion: number, edits: vscode.TextEdit[]) {
+	const textEditor = vscode.window.activeTextEditor;
+	if (textEditor && textEditor.document.uri.toString() === uri) {
+		if (textEditor.document.version !== documentVersion) {
+			vscode.window.showInformationMessage(
+				`SqlLint fixes are outdated and can't be applied to the document.`,
+			);
+		}
+		textEditor.edit((mutator) => {
+			for (const edit of edits) {
+				mutator.replace(client.protocol2CodeConverter.asRange(edit.range), edit.newText);
+			}
+		}).then((success) => {
+			if (!success) {
+				vscode.window.showErrorMessage(
+					"Failed to apply SqlLint fixes to the document. " +
+					"Please consider opening an issue with steps to reproduce.",
+				);
+			}
+		});
+	}
+}
+	context.subscriptions.push(
+		client.start(),
+		vscode.commands.registerCommand("_tsql-lint.change", applyTextEdits)
+	);
 }
